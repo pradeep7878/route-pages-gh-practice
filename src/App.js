182da113f89880d660f25bc8603e159d6d2e1d15
pradeep7878 <pradivpj@gmail.com>
2024-11-25 11:03:00 +0530

    UPDATE : gh files added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Profile from './Profile';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />}>
           <Route path='about' element={<About />} />
